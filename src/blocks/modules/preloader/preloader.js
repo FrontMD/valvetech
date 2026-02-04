dd059f134d19980b37d5d45320769b0c7306cc7a
FrontMD <web@mockup.digital>
2026-02-04 18:45:12 +0300

    preloader completed

--- src/blocks/modules/preloader/preloader.js
+++ src/blocks/modules/preloader/preloader.js
@@ -9,9 +9,35 @@ function preloader() {
     async function preloaderAnim() {
         const bg = preloader.querySelector('[data-js="preloaderBg"]')
         const text = preloader.querySelector('[data-js="preloaderText"]')
+        const textList = text?.querySelectorAll('span')
+
+        if(text) {
+            text.addEventListener('transitionend', async function() {
+                if(textList.length) {
+                    for(let i = 0; i < textList.length; i++) {
+                        textList[i].classList.add('animated')
+                        await delay(200)
+                    }
+                }
+                endPreloader()
+            })
+        } else {
+            endPreloader(1600)
+        }
+
+        await delay(500)
 
         bg?.classList.add('animated')
         text?.classList.add('animated')
+
+    }
+
+    async function endPreloader(coeff = 0) {
+        await delay(1000 + coeff)
+        preloader.style.opacity = '0',
+        await delay(1000)
+        preloader.remove()
+        unlockBody()
     }
 
 }
