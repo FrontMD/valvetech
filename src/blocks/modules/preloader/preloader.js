6c9683fab91c94aa3a87b2ba16a385ae5e94ec6c
FrontMD <web@mockup.digital>
2026-03-13 15:46:34 +0300

    preloader fix

--- src/blocks/modules/preloader/preloader.js
+++ src/blocks/modules/preloader/preloader.js
@@ -1,48 +1,59 @@
 function preloader() {
     const preloader = document.querySelector('[data-js="preloader"]')
 
-    if(!preloader) return
+    if(preloader) {
+        lockBody()
+        preloaderAnim()
 
-    lockBody()
-    preloaderAnim()
+        async function preloaderAnim() {
+            const bg = preloader.querySelector('[data-js="preloaderBg"]')
+            const text = preloader.querySelector('[data-js="preloaderText"]')
+            const textList = text?.querySelectorAll('span')
 
-    async function preloaderAnim() {
-        const bg = preloader.querySelector('[data-js="preloaderBg"]')
-        const text = preloader.querySelector('[data-js="preloaderText"]')
-        const textList = text?.querySelectorAll('span')
-
-        if(text) {
-            text.addEventListener('transitionend', async function() {
-                if(textList.length) {
-                    for(let i = 0; i < textList.length; i++) {
-                        textList[i].classList.add('animated')
-                        await delay(200)
+            if(text) {
+                text.addEventListener('transitionend', async function() {
+                    if(textList.length) {
+                        for(let i = 0; i < textList.length; i++) {
+                            textList[i].classList.add('animated')
+                            await delay(200)
+                        }
                     }
-                }
-                endPreloader()
-            })
-        } else {
-            endPreloader(1600)
+                    endPreloader()
+                })
+            } else {
+                endPreloader(1600)
+            }
+
+            await delay(500)
+
+            bg?.classList.add('animated')
+            text?.classList.add('animated')
+
         }
 
-        await delay(500)
+        async function endPreloader(coeff = 0) {
+            await delay(1000 + coeff)
+            preloader.style.opacity = '0',
+            await delay(1000)
+            AOS.init({
+                offset: 100,
+                duration: 600,
+                delay: 100,
+            });
+            preloader.remove()
+            unlockBody()
+        }
 
-        bg?.classList.add('animated')
-        text?.classList.add('animated')
-
-    }
-
-    async function endPreloader(coeff = 0) {
-        await delay(1000 + coeff)
-        preloader.style.opacity = '0',
-        await delay(1000)
-        AOS.init({
-            offset: 100,
-            duration: 600,
-            delay: 100,
-        });
-        preloader.remove()
-        unlockBody()
+    } else {
+        noPreloader()
+        async function noPreloader() {
+            await delay(500)
+            AOS.init({
+                offset: 100,
+                duration: 600,
+                delay: 100,
+            });
+        }
     }
 
 }
